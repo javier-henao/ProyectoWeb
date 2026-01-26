b9d8a428e925654324efcd240e6791291dff392f
Javier Henao <javher@MacBook-Air-de-Javier.local>
2026-01-26 02:16:33 -0500

    Organizacion y separacion de servicios, mejora en visualizacion

--- ProjectoWeb/urls.py
+++ ProjectoWeb/urls.py
@@ -21,5 +21,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     
+    path('servicios/', include('servicios.urls')),
+    
     path('', include('ProyectoWebApp.urls'))
 ]
