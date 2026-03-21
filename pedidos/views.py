7ead17b4eb8e1268f0bba5e2bff74ad0a24a48ea
Javier Henao <javier.henao@outlook.com>
2026-03-21 06:57:58 -0500

    finalizacion proyecto

--- pedidos/views.py
+++ pedidos/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth.decorators import login_required
 from pedidos.models import Pedido, LineaPedido
 from django.contrib import messages
 from django.shortcuts import redirect
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.core.mail import send_mail
 
 from carro.carro import Carro
 
@@ -23,7 +26,7 @@ def procesar_pedido(request):
     
     LineaPedido.objects.bulk_create(lineas_pedido)
     
-    enviar_email(
+    enviar_mail(
         pedido=pedido,
         lineas_pedido=lineas_pedido,
         nombre_usuario=request.user.username,
@@ -31,8 +34,19 @@ def procesar_pedido(request):
     )
     
     messages.success(request, "El pedido se ha creado correctamente")
-    return redirect('../tienda')
+    return redirect('../Tienda')
     
-def enviar_email(**kwargs):
-    pass
+def enviar_mail(**kwargs):
+    asunto="Gracias por el pedido"
+    mensaje=render_to_string('mails/pedido.html', {
+        "pedido": kwargs.get('pedido'),
+        "lineas_pedido": kwargs.get('lineas_pedido'),
+        "nombre_usuario": kwargs.get('nombre_usuario'),
+        # "email_usuario": kwargs.get('email_usuario'),
+    })
     
+    mensaje_texto=strip_tags(mensaje)
+    from_email=""
+    to=kwargs.get('email_usuario')
+    # to=kwargs.get('javier.henao@outlook.com')
+    send_mail(asunto, mensaje_texto, from_email, [to], html_message=mensaje)
